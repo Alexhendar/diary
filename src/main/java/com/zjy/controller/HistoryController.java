7489605d2e45dfc20060e4b12fc262ac625ba76f
张俊永 <junyong62@163.com>
2014-01-13 18:21:25 +0800

    查找历史日记

--- src/main/java/com/zjy/controller/HistoryController.java
+++ src/main/java/com/zjy/controller/HistoryController.java
@@ -1,13 +1,24 @@
 package com.zjy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.zjy.domain.Diary;
+import com.zjy.service.DiaryService;
+
 @Controller
 public class HistoryController {
+	@Autowired
+	private DiaryService diaryService;
+	
 	@RequestMapping(value="/history",method=RequestMethod.GET)
-	public String history(){
+	public String history(ModelMap model){
+		Page<Diary> page = diaryService.listDiaryByPage(0, 20);
+		model.addAttribute("page", page);
 		return "history";
 	}
 }
--- src/main/java/com/zjy/service/DiaryService.java
+++ src/main/java/com/zjy/service/DiaryService.java
@@ -1,8 +1,20 @@
 package com.zjy.service;
 
 import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +25,50 @@ import com.zjy.repository.DiaryRepository;
 public class DiaryService {
 	@Autowired
 	private DiaryRepository diaryRepository;
-	
+
 	@Transactional
-	public void save(Diary diary){
+	public void save(Diary diary) {
 		diary.setDate(Calendar.getInstance().getTime());
 		diaryRepository.save(diary);
+	}
+
+	/**
+	 * 
+	 * \brief listDiaryByPage根据条件分页查询日记
+	 * @param startTime
+	 * @param endTime
+	 * @param start
+	 * @param size
+	 * @return
+	 * @attention 方法的使用注意事项 
+	 * @author zhangjunyong
+	 * @date 2014年1月13日 
+	 */
+	public Page<Diary> listDiaryByPage(final Date startTime,final Date endTime,int start,int size){
+		
+		 
+		return diaryRepository.findAll(new Specification<Diary>() {
+			@Override
+			public Predicate toPredicate(Root<Diary> root, CriteriaQuery<?> query,
+					CriteriaBuilder cb) {
+				root = query.from(Diary.class);
+				Path<Date> date = root.get("date");
+				return cb.between(date, startTime, endTime);
+			}
+		},  new PageRequest(start, size, new Sort(Direction.DESC, new String[] { "date" })));
+	}
+	/**
+	 * 
+	 * \brief listDiaryByPage 分页查询日记  
+	 * @param start
+	 * @param size
+	 * @return
+	 * @attention 方法的使用注意事项 
+	 * @author zhangjunyong
+	 * @date 2014年1月13日 
+	 * @note  begin modify by 修改人 修改时间   修改内容摘要说明
+	 */
+	public Page<Diary> listDiaryByPage(int start,int size){
+		return diaryRepository.findAll(new PageRequest(start, size, new Sort(Direction.DESC, new String[] { "date" })));
 	}
 }
